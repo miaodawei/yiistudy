27e0aa0de946a761494b51d32213a6561883683a
Miao Dawei <work_style@163.com>
2020-01-03 23:44:20 +0800

    all

---
+++ modules/algorithms/services/MaxArea.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\modules\algorithms\services;
+
+class MaxArea {
+
+    public function maxArea($height) {
+        $max = $currentMin = 0;
+        $left = 0;
+        $right = count($height) - 1;
+        while(true) {
+            if($left == $right) {
+                break;
+            }
+            $min = $height[$left] < $height[$right] ? $height[$left ++] : $height[$right --];
+            if($min < $currentMin) {
+                continue;
+            }
+            $currentMin = $min;
+            $area = ($right - $left + 1) * $min;
+            $max = $max > $area ? $max : $area;
+        }
+        return $max;
+    }
+}
